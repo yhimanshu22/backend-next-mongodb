65b68ad86a4d37872191a8455cc6015779683ec2
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-05-08 03:57:04 +0530

    handling signin with authjs

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>Dashboard</div>
+    )
+}
+
+export default page
---
+++ src/app/u/username/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>message page</div>
+    )
+}
+
+export default page
